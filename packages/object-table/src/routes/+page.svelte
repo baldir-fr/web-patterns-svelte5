c680f74753482a5a188e9c828679e4b2c0eabbf0
Marc Bouvier <m.bouvier.dev@gmail.com>
2025-03-12 20:29:21 +0100

    Version 0.0.7 : Installation guide and usage with css

--- packages/object-table/src/routes/+page.svelte
+++ packages/object-table/src/routes/+page.svelte
@@ -1,6 +1,12 @@
+<!-- tag::svelte-import-css[] -->
 <script lang="ts">
+    // Use bundled style :
+    // import  "svelte-object-table/object-table.css";
+    // Or your own :
+    import "../object-table.css"
     import {ObjectTable} from "$lib/index.js";
 </script>
+<!-- end::svelte-import-css[] -->
 
 <h1>ObjectTable</h1>
 <ObjectTable data={{"Border color":"is overriden with CSS cascade"}}></ObjectTable>
