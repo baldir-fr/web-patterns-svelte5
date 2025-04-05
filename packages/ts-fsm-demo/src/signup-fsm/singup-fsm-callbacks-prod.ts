b6a015b15dec73253be30c6027683c7022575406
Marc Bouvier <m.bouvier.dev@gmail.com>
2025-04-05 19:52:26 +0200

    documentation for finite state machine callback

--- packages/ts-fsm-demo/src/signup-fsm/singup-fsm-callbacks-prod.ts
+++ packages/ts-fsm-demo/src/signup-fsm/singup-fsm-callbacks-prod.ts
@@ -2,7 +2,8 @@ import {SignupFsmContext, SignupFsmCallbacks} from "./signup-fsm";
 import {ILogger} from "typescript-fsm";
 
 const logger: ILogger = console
-
+// Here we can implement callbacks for signup finite state machine
+// They can do side effects and depend on infrastructure (database, filesystem, mail ...)
 const prodCallbacks: SignupFsmCallbacks = {
     askEmailConfirmation: async (context: SignupFsmContext) => {
         logger.info("In callback : " + this)
